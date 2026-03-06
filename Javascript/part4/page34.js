f40bd0cbf957264b743be7ba58a8eb714d2eaec2
SHARANYA-216 <sharanyasrinivas216@gmail.com>
2026-03-06 17:28:34 +0530

    new files added

--- Javascript/part4/page34.js
+++ Javascript/part4/page34.js
@@ -1,27 +1,67 @@
-//embedded javascript code
-
 import express from "express"
-const app = express()
+import session from "express-session"
+import expressLayouts from "express-ejs-layouts";
+
+const app =express();
+app.use(expressLayouts)
+app.use(express.static("public"))
+app.set("layout", "layout")
 app.set("view engine","ejs")
 app.set("views","views")
-app.listen(5000,()=>console.log('Server Started'))
+app.listen(8080,()=>console.log('Server Started'))
 
-const users = [
-    { id:1, name: "sharanya" , email: "sharanya@gmail.com", password: "1234"},
-    { id:2, name: "Rani" , email: "rani@gmail.com", password: "4321"},
-    { id:3, name: "charitha" , email: "cherry@gmail.com", password: "5678"},
+app.use(express.urlencoded({extended:true}))
+
+let users = [
+  { name: "sharanya", email: "sharanya@gmail.com", password: "1234" },
+  { name: "rani", email: "rani@gmail.com", password: "1234" },
+  { name: "charitha", email: "charitha@gmail.com", password: "1234" },
 ];
 
-//3 routes 3 pages
+app.use(
+  session({
+    secret:'mySecretKey',
+    resave:false,
+    saveUninitialized:false,
+  }),
+);
 
 app.get("/login",(req,res)=>{
-    res.render("login");
+    res.render("login",{error:null});
+})
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (user) {
+    if (user.password === password) {
+      req.session.user=user//inspect-app-cookie session id created
+      res.redirect("/");
+    } else {
+      res.render("login", { error: "Invalid Password" });
+    }
+  } else {
+    res.render("login", { error: "User not found" });
+  }
+  // res.redirect("/");
 });
 
 app.get("/register",(req,res)=>{
     res.render("register");
-});
+})
+
+
+app.post("/register",(req,res)=>{
+    //const {name,email,password}=req.body
+    users=[...users,req.body]
+    res.redirect("/")
+})
+
 
 app.get("/",(req,res)=>{
-    res.render("dashboard", { users });
+  // if(req.session.user){
+  //   res.render("dashboard",{users});
+  // }
+  res.render("dashboard", { users })
 });
+
